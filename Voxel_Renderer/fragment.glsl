2b1a71daa8cf538c0d2897f7268f341d82ba81ae
Telo <telo.philippe@telecom-paris.fr>
2023-04-19 00:35:20 +0200

    Added Camera movement

--- Voxel_Renderer/fragment.glsl
+++ Voxel_Renderer/fragment.glsl
@@ -3,6 +3,13 @@
 out vec4 FragColor;
 in vec2 fragPos;
 
+uniform vec2 u_Resolution;
+
+uniform float u_Time;
+
+uniform mat4 u_InverseProjection;
+uniform mat4 u_InverseView;
+
 struct Sphere {
 	vec3 pos;
 	float radius;
@@ -17,12 +24,13 @@ struct intersection {
 	bool hit;
 };
 
-const int numSpheres = 3;
+const int numSpheres = 4;
 
 Sphere spheres[numSpheres] = Sphere[](
-	Sphere(vec3(-2, 0, 0), .9, vec3(1, 0.1, 0.1)),
-	Sphere(vec3( 0, 0, 0), .9, vec3(0.1, 1, 0.1)),
-	Sphere(vec3( 2, 0, 0), .9, vec3(0.1, 0.1, 1))
+	Sphere(vec3(-2, -2, 0), .9, vec3(1, 0.1, 0.1)),
+	Sphere(vec3( -2, 2, 0), .9, vec3(0.1, 1, 0.1)),
+	Sphere(vec3( 2, 2, 0), .9, vec3(0.1, 0.1, 1)),
+	Sphere(vec3( 2, -2, 0), .9, vec3(0.1, 0.1, 1))
 );
 
 float sphereIntersect(Sphere sphere, vec3 pos, vec3 dir) {
@@ -36,19 +44,23 @@ float sphereIntersect(Sphere sphere, vec3 pos, vec3 dir) {
 
 void main() {
 	
-	vec2 uv = fragPos;
-	vec3 pos = vec3(0, 0, -4);
-	vec3 dir = normalize(vec3(uv, 1));
+	vec2 ScreenSpace = (gl_FragCoord.xy) / u_Resolution.xy;
+	vec4 Clip = vec4(ScreenSpace.xy * 2.0f - 1.0f, -1.0, 1.0);
+	vec4 Eye = vec4(vec2(u_InverseProjection * Clip), -1.0, 0.0);
+	vec3 RayDirection = vec3(u_InverseView * Eye);
+	vec3 RayOrigin = u_InverseView[3].xyz;
+	RayDirection = normalize(RayDirection);
+	
 	vec3 col = vec3(0);
 
 	intersection closest;
 	closest.t = 1000000;
 
 	for (int i = 0; i < numSpheres; i++) {
-		float t = sphereIntersect(spheres[i], pos, dir);
+		float t = sphereIntersect(spheres[i], RayOrigin, RayDirection);
 		if (t > 0 && t < closest.t) {
 			closest.t = t;
-			closest.pos = pos + dir * t;
+			closest.pos = RayOrigin + RayDirection * t;
 			closest.normal = normalize(closest.pos - spheres[i].pos);
 			closest.color = spheres[i].color;
 			closest.hit = true;
@@ -61,10 +73,9 @@ void main() {
 		vec3 lightCol = vec3(1, 1, 1);
 		vec3 specCol = vec3(1, 1, 1) * 4;
 		float diff = max(dot(closest.normal, lightDir), 0.0);
-		float spec = pow(max(dot(reflect(-lightDir, closest.normal), -dir), 0.0), 64);
+		float spec = pow(max(dot(reflect(-lightDir, closest.normal), -RayDirection), 0.0), 64);
 
 		col = closest.color * (diff * lightCol + spec * specCol);
-
 	}
 
 	FragColor = vec4(col, 1);
--- Voxel_Renderer/main.cpp
+++ Voxel_Renderer/main.cpp
@@ -10,20 +10,112 @@
 #include <fstream>
 #include <sstream>
 
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 // Link opengl32.lib, glfw3.lib and glew32.lib
 #pragma comment(lib, "opengl32.lib")
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "glew32.lib")
 
+int width = 800;
+int height = 800;
+
 struct Camera {
 	glm::vec3 position;
-	glm::vec3 position;
+	glm::vec3 direction;
+	float rotationX;
+	float rotationY;
+
+	glm::vec3 worldUp;
+
+	glm::vec3 front;
+	glm::vec3 right;
+	glm::vec3 up;
+
+	float lastX;
+	float lastY;
+
+	glm::mat4 projection;
+	glm::mat4 view;
 };
 
+float degtorad = 3.141592f / 180.0f;
+
+Camera camera;
+
+void cameraMouseCallback(GLFWwindow *window, const double posX, const double posY) {
+
+	const float offset_x = posX - camera.lastX;
+	const float offset_y = posY - camera.lastY;
+
+	camera.lastX = width / 2;
+	camera.lastY = height / 2;
+
+	const float sensitivity = 0.2f;
+
+	camera.rotationY += offset_x * sensitivity;
+	camera.rotationX -= offset_y * sensitivity;
+
+	if (camera.rotationX > 89.0f)
+		camera.rotationX = 89.0f;
+	if (camera.rotationX < -89.0f)
+		camera.rotationX = -89.0f;
+
+
+	camera.direction.x = cos(camera.rotationY * degtorad) * cos(camera.rotationX * degtorad);
+	camera.direction.y = sin(camera.rotationX * degtorad);
+	camera.direction.z = sin(camera.rotationY * degtorad) * cos(camera.rotationX * degtorad);
+	camera.front = glm::normalize(camera.direction);
+	camera.right = glm::normalize(glm::cross(camera.front, camera.worldUp));
+	camera.up = glm::normalize(glm::cross(camera.right, camera.front));
+
+	glfwSetCursorPos(window, width / 2, height / 2);
+
+}
+
+void setUniformM4(const unsigned int shader, const char* name, glm::mat4 matrix) {
+	const unsigned int location = glGetUniformLocation(shader, name);
+	glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
+}
+void setUniformV3(const unsigned int shader, const char* name, glm::vec3 vector) {
+	const unsigned int location = glGetUniformLocation(shader, name);
+	glUniform3fv(location, 1, glm::value_ptr(vector));
+}
+void setUniformF(const unsigned int shader, const char* name, float value) {
+	const unsigned int location = glGetUniformLocation(shader, name);
+	glUniform1f(location, value);
+}
+void setUniformV2(const unsigned int shader, const char* name, glm::vec2 vector) {
+	const unsigned int location = glGetUniformLocation(shader, name);
+	glUniform2fv(location, 1, glm::value_ptr(vector));
+}
+
+
+void initCamera() {
+	camera.position = glm::vec3(0.0f, 0.0f, -5.0f);
+	camera.direction = glm::vec3(0.0f, 0.0f, 1.0f);
+	camera.rotationX = 0.0f;
+	camera.rotationY = 0.0f;
+	camera.worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+	camera.front = glm::normalize(camera.direction);
+	camera.right = glm::normalize(glm::cross(camera.front, camera.worldUp));
+	camera.up = glm::normalize(glm::cross(camera.right, camera.front));
+	camera.lastX = width / 2;
+	camera.lastY = height / 2;
+	camera.projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+	camera.view = glm::lookAt(camera.position, camera.position + camera.front, camera.up);
+}
+
+std::string loadFile(std::string filename) {
+	std::ifstream t(filename);
+	std::stringstream buffer;
+	buffer << t.rdbuf();
+	return buffer.str();
+}
 
 int main() {
 	// Initialize GLFW
@@ -76,38 +168,50 @@ int main() {
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
 
-	// Loading the shaders 
+	// Loading the shaders and checking for compilation errors
 
-	std::ifstream vertexShaderFile("vertexShader.glsl");
-	std::stringstream vertexShaderBuffer;
-	vertexShaderBuffer << vertexShaderFile.rdbuf();
+	std::string vertexShaderSource = loadFile("vertex.glsl");
+	std::string fragmentShaderSource = loadFile("fragment.glsl");
 
-	std::ifstream fragmentShaderFile("fragmentShader.glsl");
-	std::stringstream fragmentShaderBuffer;
-	fragmentShaderBuffer << fragmentShaderFile.rdbuf();
+	const char* vertexShaderSourceC = vertexShaderSource.c_str();
+	const char* fragmentShaderSourceC = fragmentShaderSource.c_str();
 
-	std::string vertexShaderSource = vertexShaderBuffer.str();
-	std::string fragmentShaderSource = fragmentShaderBuffer.str();
-
-
-	const char* vertexShaderSourcePtr = vertexShaderSource.c_str();
-	const char* fragmentShaderSourcePtr = fragmentShaderSource.c_str();
-
-	glShaderSource(vertexShader, 1, &vertexShaderSourcePtr, NULL);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSourcePtr, NULL);
-
+	glShaderSource(vertexShader, 1, &vertexShaderSourceC, NULL);
 	glCompileShader(vertexShader);
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cerr << "Failed to compile vertex shader: " << infoLog << std::endl;
+	}
+
+	glShaderSource(fragmentShader, 1, &fragmentShaderSourceC, NULL);
 	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cerr << "Failed to compile fragment shader: " << infoLog << std::endl;
+	}
 
 	glAttachShader(shader, vertexShader);
 	glAttachShader(shader, fragmentShader);
-
 	glLinkProgram(shader);
+	glGetProgramiv(shader, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shader, 512, NULL, infoLog);
+		std::cerr << "Failed to link shader program: " << infoLog << std::endl;
+	}
 
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	glUseProgram(shader);
+	initCamera();
+
+
+	// Callbacks
+
+	glfwSetCursorPosCallback(window, cameraMouseCallback);
 
 	// Main rendering / event loop
 	while (!glfwWindowShouldClose(window)) {
@@ -116,7 +220,17 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Draw the main quad
-		
+		glUseProgram(shader);
+
+		camera.projection = glm::perspective(glm::radians(70.0f), (float)width / (float)height, 0.1f, 100.0f);
+		camera.view = glm::lookAt(camera.position, camera.position + camera.front, camera.up);
+
+		setUniformV2(shader, "u_Resolution", glm::vec2(width, height));
+		setUniformF(shader, "u_Time", glfwGetTime());
+
+		setUniformM4(shader, "u_InverseView", glm::inverse(camera.view));
+		setUniformM4(shader, "u_InverseProjection", glm::inverse(camera.projection));
+
 		glBindVertexArray(vao);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 		glBindVertexArray(0);
